a058f222599277c28c812da4483a69aa46aa53b5
Haylton Rodrigues <haylton2012@gmail.com>
2021-07-26 20:37:10 -0300

    Configuração de validação de mensagens de erro padrão

--- src/main/java/io/github/hayltondev/clientes/rest/dto/ServicoPrestadoDTO.java
+++ src/main/java/io/github/hayltondev/clientes/rest/dto/ServicoPrestadoDTO.java
@@ -2,14 +2,25 @@ package io.github.hayltondev.clientes.rest.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
 public class ServicoPrestadoDTO {
 
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 }
