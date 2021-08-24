bb7022e6f74fecb08b2f7146a4a2f40d2b4f9145
Haylton Rodrigues <haylton2012@gmail.com>
2021-08-23 22:28:02 -0300

    Configurando o AuthorizationServer

--- src/main/java/io/github/hayltondev/clientes/config/AuthorizationServerConfig.java
+++ src/main/java/io/github/hayltondev/clientes/config/AuthorizationServerConfig.java
@@ -1,4 +1,43 @@
 package io.github.hayltondev.clientes.config;
 
-public class AuthorizationServerConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Bean
+    private TokenStore tokenStore(){
+        return new InMemoryTokenStore();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager); //vem da injeção SecurityConfig pois lá ele foi configurado
+    }
+
+    @Override /*aqui onde é feita a configuração de aplicação. Nesse caso é a minha app Angular, porém se quiser outras tenho que configurar aqui também. POsso configurar tanto em memoria quanto via banco de dados*/
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("my-angular-app")
+                .secret("@321")
+                .scopes("read", "write")
+                .authorizedGrantTypes("password")
+                .accessTokenValiditySeconds(1800);
+    }
 }
